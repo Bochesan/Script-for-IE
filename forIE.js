1be1afdd04bcfd83795031d707fefc0e93a3f566
Pavel <bochesan@gmail.com>
2019-04-17 13:44:13 +0300

    Init

---
+++ forIE.js
@@ -0,0 +1,26 @@
+function getInternetExplorerVersion(){
+    var rv = -1;
+    if (navigator.appName == 'Microsoft Internet Explorer')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    else if (navigator.appName == 'Netscape')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("Trident/.*rv:([0-9]{1,}[\.0-9]{0,})");
+        if (re.exec(ua) != null)
+            rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
+}
+    
+if(getInternetExplorerVersion()!==-1){
+    // In IE
+    $("*[data-src]").addClass("visible");
+    $("*[data-src]").attr("src", function(){
+        return $(this).attr("data-src");
+    });
+}
